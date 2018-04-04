3d3519b80310639a4f7ae242526013d6c75d7759
junindar <junindar@gmail.com>
2018-04-04 10:27:02 +0700

    new

---
+++ MVCPerpustakaan/MVCPerpustakaan/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVCPerpustakaan.Startup))]
+namespace MVCPerpustakaan
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
