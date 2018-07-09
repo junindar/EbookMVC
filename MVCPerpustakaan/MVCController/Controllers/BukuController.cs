02fcd159ba5791343cdcd956f25894aaf1856dc9
junindar <junindar@gmail.com>
2018-07-09 17:27:56 +0700

    Controller

---
+++ MVCPerpustakaan/MVCController/Controllers/BukuController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCController.Controllers
+{
+    public class BukuController : Controller
+    {
+        // GET: Buku
+        public string Index()
+        {
+            return "Ini Action Index";
+        }
+
+        public string Browse()
+        {
+            return "Ini Action Browse";
+        }
+
+        public string Details()
+        {
+            return "Ini Action Details";
+        }
+    }
+}
---
+++ MVCPerpustakaan/MVCController/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCController.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Ini adalah page About. Latihan pengenalan Controller pada ASP.NET MVC";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "junindar@gmail.com";
+
+            return View();
+        }
+    }
+}
