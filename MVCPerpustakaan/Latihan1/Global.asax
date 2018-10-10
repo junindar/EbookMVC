80d60c0dbe6ccc5b9aa7554a39c83dafeada0b41
junindar <junindar@gmail.com>
2018-10-10 11:51:38 +0700

    chapter 1

---
+++ MVCPerpustakaan/Latihan1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Latihan1.MvcApplication" Language="C#" %>
---
+++ MVCPerpustakaan/Latihan1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Latihan1.Startup))]
+namespace Latihan1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
